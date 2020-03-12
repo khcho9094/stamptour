e032a2f08a0f7ad0aab5ca4e60512498ef1a13b5
조건희 <whrjsgml111@naver.com>
2020-03-12 15:09:56 +0900

    이미지 크기조정

--- src/components/TourPlace.vue
+++ src/components/TourPlace.vue
@@ -1,70 +1,95 @@
 <template>
   <div>
     <div class="sur_place">
-          <div class="title">
-              <h1> <img src="@/assets/images/icon_t_1.png" alt="icon"> 주변 축제 공연행사</h1>
-              <a href="/tour/list?type=T"><span>모두보기 <img src="@/assets/images/arrow_1.png" alt="arr"></span></a>
-          </div>
-          <ul class="list">
-              <li v-for="(data, idx) in TourData.slice(0, 2)" v-bind:key="idx" >
-                  <a :href=data.apiUrl><img :src=data.image alt=""></a>
-                  <h2>{{data.title}}</h2>
-                  <p>여기에서 {{data.dist}}km</p>
-              </li>
-          </ul>
-      </div>
-      <div class="sur_place">
-          <div class="title">
-              <h1> <img src="@/assets/images/icon_t_2.png" alt="icon"> 주변 음식점</h1>
-              <a href="/tour/list?type=F"><span>모두보기 <img src="@/assets/images/arrow_1.png" alt="arr"></span></a>
-          </div>
-          <ul class="list">
-              <li v-for="(data, idx) in FoodData.slice(0, 2)" v-bind:key="idx" >
-                  <a :href=data.apiUrl><img :src=data.image alt=""></a>
-                  <h2>{{data.title}}</h2>
-                  <p>여기에서 {{data.dist}}km</p>
-              </li>
-          </ul>
-      </div>
-      <div class="sur_place">
-          <div class="title">
-              <h1> <img src="@/assets/images/icon_t_3.png" alt="icon"> 주변 숙박업소</h1>
-              <a href="/tour/list?type=LM"><span>모두보기 <img src="@/assets/images/arrow_1.png" alt="arr"></span></a>
-          </div>
-          <ul class="list">
-              <li v-for="(data, idx) in LodgMentData.slice(0, 2)" v-bind:key="idx" >
-                  <a :href=data.apiUrl><img :src=data.image alt=""></a>
-                  <h2>{{data.title}}</h2>
-                  <p>여기에서 {{data.dist}}km</p>
-              </li>
-          </ul>
-      </div>
-      <div class="sur_place">
-          <div class="title">
-              <h1> <img src="@/assets/images/icon_t_4.png" alt="icon"> 주변 체험관광</h1>
-              <a href="/tour/list?type=L"><span>모두보기 <img src="@/assets/images/arrow_1.png" alt="arr"></span></a>
-          </div>
-          <ul class="list">
-              <li v-for="(data, idx) in LeportsData.slice(0, 2)" v-bind:key="idx" >
-                  <a :href=data.apiUrl><img :src=data.image alt=""></a>
-                  <h2>{{data.title}}</h2>
-                  <p>여기에서 {{data.dist}}km</p>
-              </li>
-          </ul>
-      </div>
-      <div class="sur_place">
-          <div class="title">
-              <h1> <img src="@/assets/images/icon_t_5.png" alt="icon"> 주변 쇼핑</h1>
-              <a href="/tour/list?type=S"><span>모두보기 <img src="@/assets/images/arrow_1.png" alt="arr"></span></a>
-          </div>
-          <ul class="list">
-              <li v-for="(data, idx) in ShoppingData.slice(0, 2)" v-bind:key="idx" >
-                  <a :href=data.apiUrl><img :src=data.image alt=""></a>
-                  <h2>{{data.title}}</h2>
-                  <p>여기에서 {{data.dist}}km</p>
-              </li>
-          </ul>
-      </div>
+        <div class="title">
+            <h1> <img src="@/assets/images/icon_t_1.png" alt="icon"> 주변 축제 공연행사</h1>
+            <a href="/tour/list?type=T"><span>모두보기 <img src="@/assets/images/arrow_1.png" alt="arr"></span></a>
+        </div>
+        <ul class="list">
+            <li v-for="(data, idx) in TourData.slice(0, 2)" v-bind:key="idx" >
+                <a :href="data.apiUrl">
+                    <div
+                        class="backImg"
+                        :style="{ 'backgroundImage': `url(${data.image})` }"
+                        ></div>
+                </a>
+                <h2>{{data.title}}</h2>
+                <p>여기에서 {{data.dist}}km</p>
+            </li>
+        </ul>
+    </div>
+    <div class="sur_place">
+        <div class="title">
+            <h1> <img src="@/assets/images/icon_t_2.png" alt="icon"> 주변 음식점</h1>
+            <a href="/tour/list?type=F"><span>모두보기 <img src="@/assets/images/arrow_1.png" alt="arr"></span></a>
+        </div>
+        <ul class="list">
+            <li v-for="(data, idx) in FoodData.slice(0, 2)" v-bind:key="idx" >
+                <a :href="data.apiUrl">
+                    <div
+                        class="backImg"
+                        :style="{ 'backgroundImage': `url(${data.image})` }"
+                        ></div>
+                </a>
+                <h2>{{data.title}}</h2>
+                <p>여기에서 {{data.dist}}km</p>
+            </li>
+        </ul>
+    </div>
+    <div class="sur_place">
+        <div class="title">
+            <h1> <img src="@/assets/images/icon_t_3.png" alt="icon"> 주변 숙박업소</h1>
+            <a href="/tour/list?type=LM"><span>모두보기 <img src="@/assets/images/arrow_1.png" alt="arr"></span></a>
+        </div>
+        <ul class="list">
+            <li v-for="(data, idx) in LodgMentData.slice(0, 2)" v-bind:key="idx" >
+                <a :href="data.apiUrl">
+                    <div
+                        class="backImg"
+                        :style="{ 'backgroundImage': `url(${data.image})` }"
+                        ></div>
+                </a>
+                <h2>{{data.title}}</h2>
+                <p>여기에서 {{data.dist}}km</p>
+            </li>
+        </ul>
+    </div>
+    <div class="sur_place">
+        <div class="title">
+            <h1> <img src="@/assets/images/icon_t_4.png" alt="icon"> 주변 체험관광</h1>
+            <a href="/tour/list?type=L"><span>모두보기 <img src="@/assets/images/arrow_1.png" alt="arr"></span></a>
+        </div>
+        <ul class="list">
+            <li v-for="(data, idx) in LeportsData.slice(0, 2)" v-bind:key="idx" >
+                <a :href="data.apiUrl">
+                    <div
+                        class="backImg"
+                        :style="{ 'backgroundImage': `url(${data.image})` }"
+                        ></div>
+                </a>
+                <h2>{{data.title}}</h2>
+                <p>여기에서 {{data.dist}}km</p>
+            </li>
+        </ul>
+    </div>
+    <div class="sur_place">
+        <div class="title">
+            <h1> <img src="@/assets/images/icon_t_5.png" alt="icon"> 주변 쇼핑</h1>
+            <a href="/tour/list?type=S"><span>모두보기 <img src="@/assets/images/arrow_1.png" alt="arr"></span></a>
+        </div>
+        <ul class="list">
+            <li v-for="(data, idx) in ShoppingData.slice(0, 2)" v-bind:key="idx" >
+                <a :href="data.apiUrl">
+                    <div
+                        class="backImg"
+                        :style="{ 'backgroundImage': `url(${data.image})` }"
+                        ></div>
+                </a>
+                <h2>{{data.title}}</h2>
+                <p>여기에서 {{data.dist}}km</p>
+            </li>
+        </ul>
+    </div>
   </div>
 </template>
 <script>
