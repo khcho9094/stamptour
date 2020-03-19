94dcedd2f7a370886fdd5d20de42ae0950379c66
tjdwhd020 <pikagen@naver.com>
2020-03-19 15:34:11 +0900

    좌표 요청 이벤트 테스트

--- src/App.vue
+++ src/App.vue
@@ -12,8 +12,25 @@ export default {
       this.$cookie.set('service_code', this.$route.query.mingleCode, 9999)
     }
   },
+  created () {
+    this.getCoordData()
+  },
   beforeMount () {
     this.$store.dispatch('setMingleCode', this.$cookie.get('service_code'))
+  },
+  methods: {
+    getCoordData () {
+      let tranggle3
+      if (this.$cookie.get('total_stamp_yn') === 'Y') {
+        if (/Android/i.test(navigator.userAgent)) {
+          tranggle3.tranggle_callback('chk_coordinate', '{}')
+        } else if (/iPhone|iPad|iPod/i.test(navigator.userAgent)) {
+          window.location = 'tranggle_callback://chk_coordinate'
+        } else {
+          return false
+        }
+      }
+    }
   }
 }
 </script>
