6fc84fb0c1291fb2dfc07d060012bd9bd61e1cf4
tjdwhd020 <pikagen@naver.com>
2020-03-26 17:43:07 +0900

    트랙 스탬프 진행률 정보 요청 이벤트 작

--- src/assets/js/app_event.js
+++ src/assets/js/app_event.js
@@ -13,6 +13,18 @@ function chkUserAgent () {
   return os
 }
 
+/* eslint-disable camelcase */
+function getCookie (name) {
+  name += '='
+  var arr = decodeURIComponent(document.cookie).split(';')
+  for (var i = 0; i < arr.length; i++) {
+    var c = arr[i]
+    while (c.charAt(0) === ' ') c = c.substring(1)
+    if (c.indexOf(name) === 0) return c.substring(name.length, c.length)
+  }
+  return ''
+}
+
 /**
  * 전자 스탬프 획득 시 안내 이벤트
  */
@@ -50,13 +62,32 @@ export function externalLinks (url) {
  */
 // eslint-disable-next-line no-unused-vars
 export function chkCoordinate () {
-  if (/Android/i.test(navigator.userAgent)) {
-    // eslint-disable-next-line no-undef
-    tranggle3.tranggle_callback('chk_coordinate', '{}')
-  } else if (/iPhone|iPad|iPod/i.test(navigator.userAgent)) {
-    window.location = 'tranggle_callback://chk_coordinate'
-  } else {
-    return false
+  var totalStampYn = getCookie('total_stamp_yn')
+  if (totalStampYn === 'Y') {
+    if (/Android/i.test(navigator.userAgent)) {
+      // eslint-disable-next-line no-undef
+      tranggle3.tranggle_callback('chk_coordinate', '{}')
+    } else if (/iPhone|iPad|iPod/i.test(navigator.userAgent)) {
+      window.location = 'tranggle_callback://chk_coordinate'
+    } else {
+      return false
+    }
+  }
+}
+
+/**
+ * 트랙 진행율 정보 요청 이벤트
+ */
+export function trackProgress () {
+  var totalStampYn = getCookie('total_stamp_yn')
+  if (totalStampYn === 'Y') {
+    var os = chkUserAgent()
+    if (os === 'android') {
+      // eslint-disable-next-line no-undef
+      tranggle3.tranggle_callback('get_track_progress', '{}')
+    } else if (os === 'ios') {
+      window.location = 'tranggle://get_track_progress'
+    }
   }
 }
 
