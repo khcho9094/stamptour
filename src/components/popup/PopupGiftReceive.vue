7e80740d28808399e6f05077e20e6fb3c53e7785
tjdwhd020 <pikagen@naver.com>
2020-06-11 11:43:23 +0900

    로그인토큰 부분 게스트 토큰 분기 추가

--- src/components/popup/PopupGiftReceive.vue
+++ src/components/popup/PopupGiftReceive.vue
@@ -128,7 +128,11 @@ export default {
                 url = 'https://drive.google.com/open?id=1LGPnKRK-Bom_v-mKo41-0kAOunyWg-rd6QI7H0ZrJR8'
               }
             } else if (this.popupGift.mingle_no === '18') {
-              url = `https://m.tranggle.com/mingle/coursebook/auth/2?token=${this.$cookie.get('login_token')}`
+              if (this.$cookie.get('login_token') !== '' && this.$cookie.get('login_token') !== null) {
+                url = `https://m.tranggle.com/mingle/coursebook/auth/2?token=${this.$cookie.get('login_token')}`
+              } else if (this.$cookie.get('guest_token') !== '' && this.$cookie.get('guest_token') !== null) {
+                url = `https://m.tranggle.com/mingle/coursebook/auth/2?token=${this.$cookie.get('guest_token')}`
+              }
               this.$store.dispatch('openPopupGift', {})
             }
             if (url) {
@@ -149,7 +153,11 @@ export default {
       return `https://m.tranggle.com/html/images/mingle/${this.popupGift.mingle_gift_image}`
     },
     durunubiCheck (ev) {
-      this.$store.dispatch('loadDurunubiCheck', this.$cookie.get('login_token'))
+      if (this.$cookie.get('login_token') !== '' && this.$cookie.get('login_token') !== null) {
+        this.$store.dispatch('loadDurunubiCheck', this.$cookie.get('login_token'))
+      } else {
+        this.$store.dispatch('loadDurunubiCheck', this.$cookie.get('guest_token'))
+      }
       setTimeout(() => {
         if (this.$store.state.durunubiCheck === '0') {
           this.durunubi = false
