786902d2d4d3a145048b743d8951a4c1d980d561
조건희 <whrjsgml111@naver.com>
2020-12-01 15:09:49 +0900

    원주 기프티콘 종료 팝업 추가

--- src/components/WonjuPopup1.vue
+++ src/components/WonjuPopup1.vue
@@ -4,26 +4,22 @@
             <img class="icon_img" src="@/assets/images/popup_icon.png" alt="icon">
             <div class="text_2 noviewpop">
                 <div class="stamp_success wonju">
-                  <b class="wonju3tit">[이벤트 당첨자 발표]</b><br>
-                  2020 원주 Smart 스탬프 투어<br>
-                  상반기 스탬프왕 당첨자 발표<br><br>
-                  <span class="tal">
-                    <b>1등 1명</b> 미니빔 프로젝터<br>
-                    <b>2등 2명</b> 스마트밴드<br>
-                    <b>3등 3명</b> 5만원 강원(원주) 상품권
-                  </span>
+                  <b class="wonju3tit">[기프티콘 발급 종료]</b><br>
+                  『2020년도 원주구석구석 어디까지 가봤니? 』<br>
+                  스탬프투어 기프티콘 발급이<br>
+                  2020년 12월 4일(금) 자정부터 종료됩니다.<br><br>
+                  2021년 새로운 모습으로 다시 만나요~
                 </div>
-                <div class="chkbox">
+                <!-- <div class="chkbox">
                   <input type="checkbox" id="noviewpop1"  name="noviewpop1" checked="checked" v-model="check"><label for="noviewpop1">다시 보지 않기</label>
-                </div>
+                </div> -->
             </div>
-            <button class="type1" @click="closeBtn">닫기</button>
-            <button class="type2" @click="viewDetail">자세히 보기</button>
+            <button @click="closeBtn">닫기</button>
+            <!-- <button class="type2" @click="viewDetail">자세히 보기</button> -->
         </div>
     </div>
 </template>
 <script>
-import * as appEvent from '@/assets/js/app_event.js'
 export default {
   name: 'WonjuPopup1',
   data () {
@@ -35,17 +31,7 @@ export default {
   },
   methods: {
     closeBtn () {
-      if (this.check) {
-        this.$cookie.set('tour_100', 'Y', 9999)
-      }
       this.$store.state.wonjuPopup1 = false
-    },
-    viewDetail () {
-      if (this.check) {
-        this.$cookie.set('tour_100', 'Y', 9999)
-      }
-      const url = 'https://docs.google.com/document/d/1SC5NK_7I1HI-ZTxEn9e-VWzocGiLGbDK-kwJE--h7N4/edit'
-      appEvent.externalLinks(url)
     }
   },
   mounted () {
