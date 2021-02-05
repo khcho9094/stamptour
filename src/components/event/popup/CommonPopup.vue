cf252a0999e83cff1da35d7678131429ca2747f6
조건희 <whrjsgml111@naver.com>
2021-02-05 11:41:52 +0900

    팝업 체크 오류 수정

--- src/components/event/popup/CommonPopup.vue
+++ src/components/event/popup/CommonPopup.vue
@@ -10,7 +10,7 @@
                   <div v-html="list.notice_popup_content"></div>
                   </div>
                   <div v-if="list.notice_popup_close_yn == 'Y'" class="chkbox">
-                  <input type="checkbox" id="noviewpop1"  name="noviewpop1" checked="checked" v-model="list.pop_check"><label for="noviewpop1">일주일 동안 보지 않기</label>
+                  <input type="checkbox" :id="`noviewpop${idx}`"  :name="`noviewpop${idx}`" checked="checked" v-model="list.pop_check"><label :for="`noviewpop${idx}`">일주일 동안 보지 않기</label>
                   </div>
               </div>
               <button :class="list.notice_popup_url ? 'type1' : ''" @click="closeBtn(list, idx)">닫기</button>
@@ -27,14 +27,6 @@
     </div>
 </template>
 <script>
-/*
-  체크해야할 것들
-  1. 한페이지 안에 팝업이 여러개일 경우 생각하기
-     현재는 commonPopupBool을 통해 한개의 팝업으로만 관리
-  2. 쿠키 저장할때 쿠키명어떻게 저장시킬지
-  3. 상세보기 링크가 외부가 아닌 내부에 레이어팝업으로 이미지만 띄울 경우
-     url 받아와서 외부에 띄우기로함
-*/
 import * as appEvent from '@/assets/js/app_event.js'
 import { mapState } from 'vuex'
 export default {
