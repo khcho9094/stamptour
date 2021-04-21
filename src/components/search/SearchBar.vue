0e972ab468d3e52bff35be0976a52a5b8d63fbd8
hyejin07 <yhhj0723@naver.com>
2021-04-21 17:53:22 +0900

    검색 추가

--- src/components/search/SearchBar.vue
+++ src/components/search/SearchBar.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="search_bar">
         <input v-model="searchInput" type="text" placeholder="검색어를 입력해 주세요.">
-        <button type="button" @click="getSearchResult(searchInput)">검색</button>
+        <button type="button" @click="getSearchInputResult(searchInput)">검색</button>
     </div>
 </template>
 <script>
@@ -10,7 +10,13 @@ export default {
   name: 'SearchBar',
   data () {
     return {
-      searchInput: ''
+      searchInput: '',
+      searchInfo: {
+        page: 1,
+        order: '', // 거리순:DISTANCE, 인기순:POP
+        stamp: '', // 방문스탬프:BADGE, 전자스탬프:STAMP, 코스스탬프:TRACK
+        view_count: 20
+      }
     }
   },
   computed: {
@@ -22,14 +28,14 @@ export default {
     }
   },
   methods: {
-    getSearchResult (searchWord) {
+    getSearchInputResult (searchWord) {
       if (searchWord === '' || searchWord === null) {
         this.$store.state.searchBool = false
         this.$store.state.searchWord = ''
         this.$router.push('/search')
       } else {
         this.$store.state.searchBool = true
-        this.$store.state.searchWord = this.searchInput
+        this.$store.state.searchWord = searchWord
         this.$store.dispatch('getSearchWord', this.searchInfo)
         this.$router.push(`/search?result=${searchWord}`)
       }
